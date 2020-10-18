c892f65f46c99234a2c67cbe296d30ee4fef2fb9
Nikolaj Volosnikoff <volosnikovkolia@gmail.com>
2020-10-18 22:15:40 +0300

    Did my part of job xD

--- classifier/urls.py
+++ classifier/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 
-urlpatterns = [
+from classifier import views
 
+urlpatterns = [
+    path('start/<int:pk>', views.start, name='start'),
+    path('start_all', views.start_all, name='start_all'),
+    path('stop/<int:pk>', views.stop, name='stop'),
+    path('stop_all', views.stop_all, name='stop_all'),
 ]
